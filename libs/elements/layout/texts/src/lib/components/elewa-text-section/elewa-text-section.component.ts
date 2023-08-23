67a0b8d55d0335b219a87537480370a48492cf31
+ <+>
2023-08-23 21:18:52 +0300

    :sparkles: created elewa-text-section component

--- libs/elements/layout/texts/src/lib/components/elewa-text-section/elewa-text-section.component.ts
+++ libs/elements/layout/texts/src/lib/components/elewa-text-section/elewa-text-section.component.ts
@@ -1,11 +1,8 @@
-import { Component, Input } from '@angular/core';
-import { HeroText } from '@elewa-website/models/schema/ui/texts';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-website-elewa-text-section',
   templateUrl: './elewa-text-section.component.html',
   styleUrls: ['./elewa-text-section.component.scss'],
 })
-export class ElewaTextSectionComponent {
-  @Input () textSection!: HeroText [];
-}
+export class ElewaTextSectionComponent {}
