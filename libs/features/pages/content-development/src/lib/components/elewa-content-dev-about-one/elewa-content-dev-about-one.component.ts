d0607400798342b27971f5bd342a5da5580f893f
+ <+>
2023-09-11 14:39:33 +0300

    :art: updated the ts file

--- libs/features/pages/content-development/src/lib/components/elewa-content-dev-about-one/elewa-content-dev-about-one.component.ts
+++ libs/features/pages/content-development/src/lib/components/elewa-content-dev-about-one/elewa-content-dev-about-one.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
+import { _highlightedImageData as highlightedImageData, getLayoutClass } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-elewa-content-dev-about-one',
   templateUrl: './elewa-content-dev-about-one.component.html',
   styleUrls: ['./elewa-content-dev-about-one.component.scss'],
 })
-export class ElewaContentDevAboutOneComponent {}
+export class ElewaContentDevAboutOneComponent {
+  @Input() imageData !: ImageAndText[];
+  imageContent: ImageAndText[];
+
+  constructor (){
+    this.imageContent = highlightedImageData;
+  }
+
+  getLayoutClass(imageContent: ImageAndText): string {
+    return getLayoutClass(imageContent);
+  }
+}
+
